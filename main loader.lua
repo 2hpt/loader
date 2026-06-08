8479ecd22963eca76146f528eb8a7e8d57ef75b4
2hpt <e0wplol@gmail.com>
2026-06-08 04:28:56 -0500

    Add files via upload

---
+++ main loader.lua
@@ -0,0 +1,8 @@
+if getgenv().loaded then 
+    return 
+end
+getgenv().loaded = true
+
+if game.PlaceId == 7711635737 then
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/854440b064fddfacb6010badaa5d9de0.lua"))()
+end
