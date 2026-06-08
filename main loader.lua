f98bdd9ef6b7ade31569f4c0e441bb7101dbc13b
2hpt <e0wplol@gmail.com>
2026-06-08 04:33:47 -0500

    Update main loader.lua

--- main loader.lua
+++ main loader.lua
@@ -1,8 +1,3 @@
-if getgenv().loaded then 
-    return 
-end
-getgenv().loaded = true
-
 if game.PlaceId == 7711635737 then
     loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/854440b064fddfacb6010badaa5d9de0.lua"))()
 end
