54bc70fa77e6de9becf97cacbe65a726b77b7c4d
2hpt <e0wplol@gmail.com>
2026-06-12 12:05:49 -0500

    Update main loader.lua
    Conditionally load script based on GameId.

--- main loader.lua
+++ main loader.lua
@@ -1,2 +1,4 @@
-loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/a7d6ea7075cf07b11a25819b7a3666e1.lua"))()
+if (game.GameId == 92416421522960) then
+  loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/a7d6ea7075cf07b11a25819b7a3666e1.lua"))()
+end
 
