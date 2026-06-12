aa676ee4cbc4c68f8599f7f4dc20bdd9b20fda77
2hpt <e0wplol@gmail.com>
2026-06-12 13:36:25 -0500

    Update main loader.lua

--- main loader.lua
+++ main loader.lua
@@ -1,6 +1,5 @@
-if game.GameId == 111958650 then
-  -- arsenal 
-elseif game.GameId == 2992873140 then
-  -- emergency hamburg 
+
+if game.GameId == 2992873140 then
+  loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/6ff1a5686f220cee19d40a3fc1802c3e.lua"))()
 end
 
