781c635ec6888013e37090628bd2f81362bbe5ab
2hpt <e0wplol@gmail.com>
2026-06-13 17:36:22 -0500

    Update main loader.lua

--- main loader.lua
+++ main loader.lua
@@ -1,4 +1,4 @@
-if game.GameId == 5995470825 then
-  loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/ee9af5de015f05a53c9d3060b4ea1ffc.lua"))()
+if game.GameId == 10314560453 then
+  loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/af84f9c843513d052c650137477c5b9c.lua"))()
 end
 
